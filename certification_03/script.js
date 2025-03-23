b8bae10cd4818fb03109c8db7e2365bbad5c22b6
vapb <vapb7123@gmail.com>
2025-03-23 20:28:25 -0300

    Finish certification_03

--- certification_03/script.js
+++ certification_03/script.js
@@ -37,24 +37,34 @@ function prepareData(data) {
     return [data['monthlyVariance'], years, months, variances]
 }
 
-function generateScales(years, months, variances) {
+function generateScales(years) {
     xScale = d3.scaleLinear()
-                .domain([d3.min(years), d3.max(years)])
+                .domain([d3.min(years), d3.max(years)+ 1])
                 .range([padding, width - padding]);
 
-    yScale = d3.scaleLinear()
-                .domain([d3.min(months) - 1, d3.max(months)])
+    yScale = d3.scaleTime()
+                .domain([new Date(0,0,0,0,0,0,0), new Date(0,12,0,0,0,0,0)])
                 .range([padding, height - padding]);
+}
 
-    colorScale = d3.scaleSequential()
-                    .domain([d3.min(variances), d3.max(variances)])
-                    .interpolator(d3.interpolateViridis);
+function colorScale(variance){
+    if (variance <= -1) {
+        return 'SteelBlue'
+    }else if (variance <= 0) {
+        return 'LightSteelBlue'
+    }else if (variance <= 1) {
+        return 'Orange'
+    }else {
+        return 'Crimson'   
+    }
 }
 
 function generateAxes() {
-    let xAxis = d3.axisBottom(xScale);
+    let xAxis = d3.axisBottom(xScale)
+                  .tickFormat(d3.format('d'));
 
-    let yAxis = d3.axisLeft(yScale);
+    let yAxis = d3.axisLeft(yScale)
+                  .tickFormat(d3.timeFormat('%B'));
 
     svg.append("g")
         .attr("id", "x-axis")
@@ -67,21 +77,43 @@ function generateAxes() {
         .call(yAxis);
 }
 
-function drawBars(data, years, months, variances) {
+function drawCells(data, baseTemp) {
+    let tooltip = d3.select('body')
+        .append('div')
+        .attr('id', 'tooltip')
+        .style('visibility', 'hidden')
+
     svg.selectAll("rect")
         .data(data)
         .enter()
         .append("rect")
         .attr("fill", "navy")
-        .attr("class", "bar")
-        .attr("x", (d, i) => xScale(years[i]))
-        .attr("y", (d, i) => yScale(months[i] - 1))
+        .attr("class", "cell")
+        .attr("x", (d, i) => xScale(d['year']))
+        .attr("y", (d, i) => yScale(new Date(0, d['month'] - 1, 0,0,0,0,0)))
         .attr("width", ((width - (2 * padding))/(data.length/12)))
         .attr("height", ((height - (2 * padding))/(12)))
-        .attr("data-xvalue", (d, i) => years[i])
-        .attr("data-yvalue", (d, i) => months[i])
+        .attr("data-month", (d, i) => d['month'] - 1)
+        .attr("data-year", (d, i) => d['year'])
+        .attr("data-temp", (d, i) => baseTemp + d['variance'])
         .style('fill', function (d, i) {
-            return colorScale(variances[i])
+            return colorScale(d['variance'])
+        })
+        .on('mouseover', (e, d) => {
+            tooltip.transition()
+                .style('visibility', 'visible');
+
+            let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+            tooltip.html(`${d['year']} ${monthNames[d['month'] -1]}` + '</br>' +  `Variance : ${d['variance']}`+ '</br>'+  `Temperature : ${baseTemp + d['variance']}`)
+                .style('left', (event.pageX + 10) + 'px')  // Position near cursor
+                .style('top', (event.pageY - 30) + 'px');  // Slightly above cursor
+                
+            tooltip.attr('data-year', d['year'])
+
+        })
+        .on('mouseout', (e, d) => {
+            tooltip.style('visibility', 'hidden')
+            //tooltip.transition().duration(200).style('opacity', 0);
         })
         ;
 }
@@ -91,11 +123,12 @@ async function main() {
     console.log('Start Main');
     drawCanvas(width, height);
     let dataset = await fetchData(url);
+    let baseTemp = dataset['baseTemperature']
     let [data, years, months, variances] = prepareData(dataset);
     console.log(data)
-    generateScales(years, months, variances);
+    generateScales(years);
     generateAxes();
-    drawBars(data, years, months, variances)
+    drawCells(data, baseTemp)
 }
 
 main()
